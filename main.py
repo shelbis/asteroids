2c7b42f81b2aed9613f54571028c9a768811e3a1
Shelbis <mantvydas970@gmail.com>
2026-02-03 13:59:00 +0100

    Modified main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -14,6 +16,8 @@ def main():
             
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
