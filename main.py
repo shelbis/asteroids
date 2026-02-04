ffaa3a74b05b3d26c72bb5d0ce950b9dddc390e3
Shelbis <mantvydas970@gmail.com>
2026-02-04 10:43:11 +0100

    Added: rotate ship

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def main():
             
         screen.fill("black")
         player1.draw(screen)
+        player1.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
